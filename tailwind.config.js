faccc3e2da07ba37ad2621f8256d60dd1ff1653d
Owen Clark <owenthomasclark@me.com>
2020-06-27 21:09:10 +0100

    Github link, email link and favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: {
     mode: 'production',
     content: ['./src/**/*.html'],
   },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'brand': {
+          100: "#fcf6e8",
+          200: "#f7e5ba",
+          300: "#f2d48c",
+          400: "#edc35e",
+          500: "#e8b230",
+          600: "#cf9917",
+          700: "#a17712",
+          800: "#73550d",
+          900: "#453308",
+        }
+      }
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/ui'),
+  ],
 }
